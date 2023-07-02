3a969a57610486959d848272208cc8dc3d2a849d
Jemal Rashidi <jmsrsd@gmail.com>
2023-07-02 22:03:21 +0700

    change theme to wopian

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init powershell --config "$env:POSH_THEMES_PATH\easy-term.omp.json" | Invoke-Expression
+oh-my-posh init powershell --config "$env:POSH_THEMES_PATH\wopian.omp.json" | Invoke-Expression
 Import-Module -Name CompletionPredictor
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadlineOption -PredictionViewStyle ListView
